832a723e9410699b118d70de11fc0c6bf347d03e
yxy <yxy3213493159@qq.com>
2025-10-14 11:44:08 +0800

    编写认证过滤网关

--- zhiyan-gateway/src/main/java/hbnu/project/zhiyangateway/ZhiyanGatewayApplication.java
+++ zhiyan-gateway/src/main/java/hbnu/project/zhiyangateway/ZhiyanGatewayApplication.java
@@ -2,8 +2,15 @@ package hbnu.project.zhiyangateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "hbnu.project.zhiyangateway",
+        "hbnu.project.zhiyancommonbasic",
+        "hbnu.project.zhiyancommonredis",
+        "hbnu.project.zhiyancommonsecurity"
+})
 public class ZhiyanGatewayApplication {
 
     public static void main(String[] args) {
--- zhiyan-gateway/src/main/java/hbnu/project/zhiyangateway/handler/SentinelFallbackHandler.java
+++ zhiyan-gateway/src/main/java/hbnu/project/zhiyangateway/handler/SentinelFallbackHandler.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Mono;
 /**
  * 自定义限流异常处理
  *
- * @author ruoyi
+ * @author yui
  */
 public class SentinelFallbackHandler implements WebExceptionHandler {
     private Mono<Void> writeResponse(ServerResponse response, ServerWebExchange exchange) {
