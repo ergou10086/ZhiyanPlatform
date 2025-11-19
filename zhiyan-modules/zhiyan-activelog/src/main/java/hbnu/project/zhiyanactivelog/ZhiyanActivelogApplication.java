c012ccd92a3b9bdd795b162fee3a7abb9a55f6a6
ErguTree <zjm88822201@126.com>
2025-11-19 15:43:54 +0800

    activelog模块的主类相关配置正确

--- zhiyan-modules/zhiyan-activelog/src/main/java/hbnu/project/zhiyanactivelog/ZhiyanActivelogApplication.java
+++ zhiyan-modules/zhiyan-activelog/src/main/java/hbnu/project/zhiyanactivelog/ZhiyanActivelogApplication.java
@@ -8,11 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author ErgouTree
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class,
+})
 public class ZhiyanActivelogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZhiyanActivelogApplication.class, args);
+        System.out.println("666，开了？？我指的是服务");
     }
 
 }
